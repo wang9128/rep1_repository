c379c14442be3ffe2fbe6ff9d7f825ccd1d726b8
wx <2062494857@qq.com>
2021-09-22 10:08:06 +0800

    DeptInfo 提交

---
+++ DeptInfo.java
@@ -0,0 +1,23 @@
+package com.ambow.entity;
+
+public class DeptInfo {
+
+    private int did ;
+    private String dname ;
+
+    public int getDid() {
+        return did;
+    }
+
+    public void setDid(int did) {
+        this.did = did;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+}
